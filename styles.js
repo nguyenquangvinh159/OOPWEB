6f036637ff27c8f7817f96740fad61b5ad9043c8
nguyenquangvinh159 <148456140+nguyenquangvinh159@users.noreply.github.com>
2024-11-12 23:05:09 +0700

    Delete styles.js

--- styles.js
+++
@@ -1,9 +0,0 @@
-function changeImage(element, newSrc) {
-    const img = element.querySelector('img');
-    img.src = newSrc; // Thay đổi đường dẫn ảnh
-}
-
-function resetImage(element, originalSrc) {
-    const img = element.querySelector('img');
-    img.src = originalSrc; // Khôi phục ảnh gốc
-}
